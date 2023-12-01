52455d57697b3180bf285649446eabfd35eb0c44
NARAYAN <narayan9deep@gmail.com>
2023-12-01 20:16:51 +0530

    logic to generate password

--- App.tsx
+++ App.tsx
@@ -19,6 +19,48 @@ export default function App() {
   const [numbers, useNumbers] = useState(false);
   const [symbols, useSymbols] = useState(false);
 
+  //generate password string:
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+    const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberDigits = '0123456789';
+    const symbolList = '!@#$%^&*()_+';
+
+    if (lowercase) {
+      characterList += lowerChars;
+    }
+    if (uppercase) {
+      characterList += upperChars;
+    }
+    if (numbers) {
+      characterList += numberDigits;
+    }
+    if (symbols) {
+      characterList += symbolList;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    isPasswordGenerated(true);
+  };
+
+  //create password:
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < characters.length; i++) {
+      // const element = array[i];
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  //reset password generator state:
+  const resetPasswordState = () => {
+    //
+  };
+
   return (
     <View>
       <Text>App</Text>
